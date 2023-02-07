55efc146a3627f083ccea4d49cfdabf841524897
ignuxas <mikolaitis.ignas@gmail.com>
2023-02-07 20:48:54 +0200

    new 404 page and style change

--- pages/404.js
+++ pages/404.js
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function Custom404() {
     return(
-        <div id="NotFound">
+        <div id="NotFound" className="flexcenter">
+            <p>NUKES N<span style={{animation: "asddMainColor 3s linear infinite"}}>'</span> SHIT</p>
             <h1>404</h1>
             <Link href="/" id="NotFoundBtn" className="flexcenter"> HOME </Link>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/AtomBomb.png" alt="404" id="NotFoundImg" />
             <div class="frame">
                 <div></div>
                 <div></div>
