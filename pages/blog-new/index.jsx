1a8b6b419781a4a837a1f8d0f25b0d7fba1e2e77
nukesnshit <122561680+nukesnshit@users.noreply.github.com>
2023-03-11 00:18:39 +0200

    Update index.jsx
    Changed "Industrial" tag to "Historical"

--- pages/blog-new/index.jsx
+++ pages/blog-new/index.jsx
@@ -32,7 +32,7 @@ export default function Blog({posts}) {
     const [activeTag, setActiveTag] = useState("All")
     const [sortedPosts, setPosts] = useState([])
 
-    const tags = ["All", "Bunkers", "Abandoned", "Industrial", "Restoration"]
+    const tags = ["All", "Bunkers", "Abandoned", "Historical", "Restoration"]
 
     // Filter posts by tag
     useEffect (() => { AOS.init() }, [])
